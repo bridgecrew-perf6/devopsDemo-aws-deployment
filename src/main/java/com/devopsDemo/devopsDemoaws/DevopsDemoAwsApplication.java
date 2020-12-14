6cf944163f1a9e969e87b6c69fc677e994f65787
Priya <piyupriyapatil@gmail.com>
2020-12-14 14:23:56 -0600

    Initial Commit

---
+++ src/main/java/com/devopsDemo/devopsDemoaws/DevopsDemoAwsApplication.java
@@ -0,0 +1,13 @@
+package com.devopsDemo.devopsDemoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devopsDemo/devopsDemoaws/DevopsDemoAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devopsDemo.devopsDemoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
